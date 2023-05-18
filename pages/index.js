9c8407780dcfd0c02670dfb73f558e3c9535dde2
Hernán <hernan@null.net>
2023-05-18 02:34:01 +0000

    dev ome setup

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
 
 function throwError() {
@@ -11,59 +9,43 @@ function throwError() {
 }
 
 function Home() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
-
   useEffect(() => {
-    const r = setInterval(() => {
-      increment()
-    }, 1000)
+    const hls = document.createElement('script');
+    hls.src = 'https://unpkg.com/hls.js@1.0.2/dist/hls.min.js';
+    document.body.append(hls);
+    
+    var script = document.createElement('script');
+    script.src = "/ovenplayer.js";
+
+    let player;
+    script.onload = () => {
+      player = OvenPlayer.create("player_el_id", {
+          "autoStart": true,
+          "autoFallback": true,
+          "mute": true,
+          "sources": [{
+              "type": "ll-hls",
+              "file": "https://llhls-demo.ovenmediaengine.com/app/stream/llhls.m3u8"
+          }]
+      });
+    };
+
+    document.body.append(script);
 
     return () => {
-      clearInterval(r)
+      document.body.removeChild(script);
+      player.remove();
     }
-  }, [increment])
+
+  }, []);
 
   return (
     <main className={styles.main}>
-      <h1>Fast Refresh Demo</h1>
-      <p>
-        Fast Refresh is a Next.js feature that gives you instantaneous feedback
-        on edits made to your React components, without ever losing component
-        state.
-      </p>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          Auto incrementing value. The counter won't reset after edits or if
-          there are errors.
-        </p>
-        <p>Current value: {count}</p>
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>Component with state.</p>
-        <ClickCount />
-      </div>
-      <hr className={styles.hr} />
-      <div>
-        <p>
-          The button below will throw 2 errors. You'll see the error overlay to
-          let you know about the errors but it won't break the page or reset
-          your state.
-        </p>
-        <Button
-          onClick={(e) => {
-            setTimeout(() => document.parentNode(), 0)
-            throwError()
-          }}
-        >
-          Throw an Error
-        </Button>
-      </div>
-      <hr className={styles.hr} />
+      <h1>
+        Ovenplayer test
+
+        <video id="player_el_id" />
+      </h1>
     </main>
   )
 }
